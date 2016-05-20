778b9dd27d67b559a46f45be40fcabe5010ece70
laithalsunni <lalsunn1@binghamton.edu>
2016-05-20 00:47:40 -0400

    Add files via upload

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+  Movie.create!(movie)
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.body =~ /.*#{e1}.*#{e2}/
+  
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +28,34 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  #Movie.all_ratings.each do |rating|
+  #uncheck "ratings[#{rating}]"
+  #end
+  #p rating_list
+  rating_list.split.each do |rating|
+  step %Q{I #{uncheck}check "ratings_#{rating}"}   
+  end
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  movie_list = Movie.all
+  #p movie_list
+  movie_list.each { |movie| step %Q{I should see "#{movie.title}"}}
+  #fail "Unimplemented"
+end
+
+Then /I should (not )?see the following movies: (.*)$/ do |not_see, movie_list|
+  # Make sure that all the movies in the app are visible in the table
+  movie_list.split(",").each do |movie|
+    movie = movie.gsub(/["']/, '').strip
+    #page.should have_content(movie)
+    if not_see
+      expect(page).to_not have_content(movie)
+    else
+      expect(page).to have_content(movie)
+    end
+    #fail "Unimplemented"
+  end
 end
